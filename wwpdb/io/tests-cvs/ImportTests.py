cec5c75c24c410018cd9d02d028554897609d746
Ezra Peisach <ezra.peisach@rcsb.org>
2018-10-10 13:59:32 -0400

    CVS tools moved

---
+++ wwpdb/io/tests-cvs/ImportTests.py
@@ -0,0 +1,34 @@
+##
+# File: ImportTests.py
+# Date:  06-Oct-2018  E. Peisach
+#
+# Updates:
+##
+"""Test cases for emdb - simply import everything to ensure imports work"""
+
+__docformat__ = "restructuredtext en"
+__author__ = "Ezra Peisach"
+__email__ = "peisach@rcsb.rutgers.edu"
+__license__ = "Creative Commons Attribution 3.0 Unported"
+__version__ = "V0.01"
+
+import unittest
+
+from wwpdb.io.cvs.CvsAdmin          import CvsAdmin,CvsSandBoxAdmin
+from wwpdb.io.cvs.CvsUtility      import CvsWrapper
+
+class ImportTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testInstantiate(self):
+        vc = CvsAdmin(tmpPath="./")
+        vc = CvsSandBoxAdmin(tmpPath="./")
+        vc = CvsWrapper(tmpPath="./")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    

409beb6db567243983e71ea295cfc66608429a5d
Ezra Peisach <ezra.peisach@rcsb.org>
2018-10-11 14:33:44 -0400

    Move DataExchange and mmCIFUtil from SVN

---
+++ wwpdb/io/tests-io/FileImportTests.py
@@ -0,0 +1,36 @@
+##
+# File: FileImportTests.py
+# Date:  10-Oct-2018  E. Peisach
+#
+# Updates:
+##
+"""Test cases for wwpdb.io.file - simply import everything to ensure imports work"""
+
+__docformat__ = "restructuredtext en"
+__author__ = "Ezra Peisach"
+__email__ = "peisach@rcsb.rutgers.edu"
+__license__ = "Creative Commons Attribution 3.0 Unported"
+__version__ = "V0.01"
+
+import unittest
+
+from wwpdb.io.file.DataExchange import DataExchange
+from wwpdb.io.file.DataFile import DataFile
+from wwpdb.io.file.mmCIFUtil import mmCIFUtil
+
+
+class ImportTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testInstantiate(self):
+        vc = DataFile()
+        vc = mmCIFUtil()
+        # Will not function without a reqObj
+        #vc = DataExchange()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    

68e402841d550956395a4f7689adba1dea3e8431
jmb <jmb@ebi.ac.uk>
2020-05-14 14:59:42 +0100

    added tests for emd

--- wwpdb/io/tests-io/ReleasePathInfoTests.py
+++ wwpdb/io/tests-io/ReleasePathInfoTests.py
@@ -41,6 +41,21 @@ class ReleasePathInfoTests(unittest.TestCase):
         self.assertTrue('modified' in ret)
         self.assertTrue(self.RPI.previous_folder_name in ret)
 
+    def test_for_release_emd_header(self):
+        rel_path = self.RPI.get_for_release_path()
+        ret = self.RPI.get_emd_subfolder_path(accession='EMD-1223', subfolder='header')
+        self.assertIsNotNone(ret)
+        self.assertNotEqual(ret, rel_path)
+        self.assertTrue('emd' in ret)
+
+    def test_for_release_emd_header_previous(self):
+        rel_path = self.RPI.get_for_release_path()
+        ret = self.RPI.get_previous_emd_subfolder_path(accession='EMD-1234', subfolder='header')
+        self.assertIsNotNone(ret)
+        self.assertNotEqual(ret, rel_path)
+        self.assertTrue('emd' in ret)
+        self.assertTrue(self.RPI.previous_folder_name in ret)
+
 
 if __name__ == '__main__':
     unittest.main()
